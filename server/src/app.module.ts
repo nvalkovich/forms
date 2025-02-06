0bbc01e584f3d4dcb1e005ad8ec3a8c5f2af8011
nvalkovich <anastasiya.valkovich@mail.ru>
2025-02-06 17:10:05 +0300

    feat: connect with database, check server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+  ],
+    controllers: [HealthController],
 })
+
 export class AppModule {}
