7cfdddb257c7d036374c4a9643bff1c406ab72e1
nvalkovich <anastasiya.valkovich@mail.ru>
2025-02-26 14:29:08 +0300

    fix: fix displaying success messages after auth

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,3 +1,5 @@
+import { User } from "./user";
+
 export enum AuthType {
     login = 'login',
     register = 'register',
@@ -7,3 +9,14 @@ export enum AuthLinksKeys {
     noAccount = 'noAccount',
     haveAccount = 'haveAccount',
 }
+
+export interface AuthFormData {
+    name?: string; 
+    email: string;
+    password: string;
+};
+
+export interface AuthResponse {
+    token: string;
+    user: User;
+};
