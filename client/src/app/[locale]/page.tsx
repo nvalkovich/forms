73ea3c696c4fdecce777082e42ca1404a7d90115
nvalkovich <anastasiya.valkovich@mail.ru>
2025-03-17 21:31:59 +0300

    feat: add temporary redirect to templates from main page

--- client/src/app/[locale]/page.tsx
+++ client/src/app/[locale]/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+
+import { useEffect } from "react";
+import { useNavigation, Routes } from "@/hooks/useNavigation";
+
 export default function HomePage() {
+    const {navigate} = useNavigation();
+
+    useEffect(() => {
+        navigate(Routes.templates);
+    })
+
     return null;
 }
